c320dec753a66f4605177ccd003adccb7f867aee
wyj_account <150566955@qq.com>
2019-08-26 00:09:13 +0800

    test8

--- save-file.php
+++ save-file.php
@@ -1,17 +1,8 @@
 <?php
-	$txt=""; 
 	$myfile = fopen("config.ini", "w") or die("Unable to open file!");
-	$txt = serialize($_POST["thehost"])+"\n";
-	echo $_POST["thehost"];
-	fwrite($myfile, $txt);
-	$txt = serialize($_POST["theuser"])+"\n";
-	echo $_POST["theuser"];
-	fwrite($myfile, $txt);
-	$txt = serialize($_POST["thepwd"])+"\n";
-	echo $_POST["thepwd"];
-	fwrite($myfile, $txt);
-	$txt = serialize($_POST["thedbname"])+"\n";
-	echo $_POST["thedbname"];
-	fwrite($myfile, $txt);
+	fwrite($myfile, $_POST["thehost"]+"\n");
+	fwrite($myfile, $_POST["theuser"]+"\n");
+	fwrite($myfile, $_POST["thepwd"]+"\n");
+	fwrite($myfile, $_POST["thedbname"]+"\n");
 	fclose($myfile);
 ?>
