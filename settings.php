46beba8f227e6940162c50f551cfcdb2a6dc140c
wyj_account <150566955@qq.com>
2019-08-31 13:12:34 +0800

    还是完善

--- settings.php
+++ settings.php
@@ -3,23 +3,16 @@
   <head>
     <title>Inventory System</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="style.css" rel="stylesheet">
   </head>
 
   <body>
   <div class="container">
-
 	<div class="row">
 		<div class="col-md-12">
-      <?php include('menu.php'); ?>
-      
-      <?php include('get-parameters.php'); ?>
-      
-			<div class="jumbotron">
-
-        <?php include ('settings-form.php');?>
-        
-      </div>
+		<?php include('menu.php'); ?>
+		<div class="jumbotron">
+			<?php include ('settings-form.php');?>
+		</div>
     </div>
   </div>
 </div>
