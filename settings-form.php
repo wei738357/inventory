5699d872cce2228f8cf3471a54fbc2f7e7d7de2a
wyj_account <150566955@qq.com>
2019-08-31 23:28:18 +0800

    完成

--- settings-form.php
+++ settings-form.php
@@ -3,20 +3,12 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 </head>
 <body>
-<form name="commentform" method="post" action="save-file.php">
-<p>
-主机/IP: <input type="text" name="thehost" style="width:70%" />
-</p>
-<p>
-用户名: <input type="text" name="theuser" />
-</p>
-<p>
-密　码: <input type="text" name="thepwd" />
-</p>
-<p>
-数据库: <input type="text" name="thedbname" />
-</p>
-<input type="submit" value="提 交" />
-</form>
+	<form name="commentform" method="post" action="save-file.php">
+		<p>Endpoint: <input type="text" name="thehost" style="width:70%" /></p>
+		<p>Username: <input type="text" name="theuser" /></p>
+		<p>Password: <input type="text" name="thepwd" /></p>
+		<p>Database: <input type="text" name="thedbname" /></p>
+		<input type="submit" value="保 存" />
+	</form>
 </body>
 </html>
